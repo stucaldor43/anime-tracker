0730d6fc107c18c03b3c096ba2c4062a2936e995
Anthony Cook <ddragonmaster49@yahoo.com>
2016-10-23 03:45:48 +0000

    Allow genre search page visitors to start another search

--- app/controllers/genre_search.rb
+++ app/controllers/genre_search.rb
@@ -2,6 +2,8 @@ get '/search/anime/genres' do
   url = "/browse/anime?genres=#{ params['genre'].join(',') }"
   res = settings.anilist_communicator.make_get_request(url)
   @search_results = get_response_body(res)
+  @title_search_partial = erb :anime_title_search_partial
+  @genre_search_form_partial = erb :anime_genre_search_form_partial
   @header = erb :header
   @footer = erb :footer
   
