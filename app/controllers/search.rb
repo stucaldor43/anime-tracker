2a14d37b71159732a28b1ac16edcf64d790ceb61
Anthony Cook <ddragonmaster49@yahoo.com>
2016-10-23 23:47:21 +0000

    Allow search page visitors to start another search

--- app/controllers/search.rb
+++ app/controllers/search.rb
@@ -3,6 +3,8 @@ get '/search/anime/:query' do
   @search_results = get_response_body(get_hummingbird_response(endpoint))
   @header = erb :header
   @footer = erb :footer
+  @title_search_partial = erb :anime_title_search_partial
+  @genre_search_form_partial = erb :anime_genre_search_form_partial
   
   erb :search
 end
