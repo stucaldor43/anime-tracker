4238f08c9eb72c8bab02a6e694f1e1eb7072bcf8
Anthony Cook <ddragonmaster49@yahoo.com>
2016-11-01 21:24:10 +0000

    Change URI used to access partial templates

--- app/api/endpoints.rb
+++ app/api/endpoints.rb
@@ -1,11 +1,11 @@
 require 'json'
 
-get '/api/anime-title-search-partial' do
+get '/api/partials/anime-title-search-partial' do
   resource_contents = erb :anime_title_search_partial
   JSON.generate({"contents" => resource_contents})
 end
 
-get '/api/anime-genre-search-form-partial' do
+get '/api/partials/anime-genre-search-form-partial' do
   resource_contents = erb :anime_genre_search_form_partial
   JSON.generate({"contents" => resource_contents}) 
 end
