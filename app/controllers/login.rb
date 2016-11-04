d57d6b389dd0c291d78373db16efb8075c780cf2
Anthony Cook <ddragonmaster49@yahoo.com>
2016-11-04 04:00:36 +0000

    Store authenticated username in session on login

--- app/controllers/login.rb
+++ app/controllers/login.rb
@@ -9,6 +9,7 @@ post '/login' do
   token = find_hummingbird_user_authentication_token(params['username'], params['password'])
   if token 
     session['auth_token'] = token
+    session['username'] = params['username']
     redirect to('/')
   end
 end
