71324cb6cd42e3918f78012945c5e84a42cbfb60
Anthony Cook <ddragonmaster49@yahoo.com>
2016-12-11 01:46:47 +0000

    Fix issue where duplicate search results would often be displayed

--- app/public/js/genre_search.js
+++ app/public/js/genre_search.js
@@ -2,15 +2,25 @@
     var affixEvent = helpers.affixEvent;
     var qs = helpers.qs;
     var makeGetRequest = helpers.makeGetRequest;
-    var pageNumber = 2;
+    
+    function createPageStatusObject() {
+        var pageNumber = 1;
+        return {
+            getPageNumber: function() {
+                pageNumber += 1;
+                return pageNumber;
+            }
+        };
+    }
+    
+    var pageStatus = createPageStatusObject();
     
     affixEvent(qs(".result-wrapper"), "scroll", function() {
-        if(this.scrollHeight - this.scrollTop <= this.clientHeight * 3) {
+        if (this.scrollHeight - this.scrollTop === this.clientHeight) {
             makeGetRequest.call(helpers, "/api/search/anime/genres" + window.location.search + 
-            "&page=" + pageNumber, function() {
+            "&page=" + pageStatus.getPageNumber(), function() {
                 if (this.status === 200) {
                     qs(".result-wrapper").innerHTML += this.response;
-                    pageNumber++;
                 }
             });
         }
