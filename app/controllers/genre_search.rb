073b7e9614f7190c6ececd44601dbb71fcb07db9
Anthony Cook <ddragonmaster49@yahoo.com>
2017-03-06 15:37:28 +0000

    Searching by genre/title will exclude titles which are part of the hentai genre

--- app/controllers/genre_search.rb
+++ app/controllers/genre_search.rb
@@ -1,14 +1,14 @@
 get '/search/anime/genres' do
   pass if (params['page'] != nil && !(params['page'].codepoints.all? {|codepoint| ((48..57) === codepoint) }))
   
-  url = "/browse/anime?genres=#{ params['genre'].join(',') }&page=#{ params['page'] || 1 }"
+  url = "/browse/anime?genres=#{ params['genre'].join(',') }&page=#{ params['page'] || 1 }&genres_exclude=Hentai"
   res = settings.anilist_communicator.make_get_request(url)
   pass if !res.kind_of? Net::HTTPSuccess
   
   @search_results = get_response_body(res)
   
   if !@search_results.empty?
-    pagination_test_url = "/browse/anime?genres=#{ params['genre'].join(',') }&page=#{ (params['page']) ? params['page'].to_i + 1 : 1 + 1 }"
+    pagination_test_url = "/browse/anime?genres=#{ params['genre'].join(',') }&page=#{ (params['page']) ? params['page'].to_i + 1 : 1 + 1 }&genres_exclude=Hentai"
     test_result = get_response_body(settings.anilist_communicator.make_get_request(pagination_test_url))
     @is_previous_button_visible = (params['page'].nil? || params['page'].to_i == 1) ? false : true
     @is_next_button_visible = (Array === test_result && !test_result.empty?) ? true : false
--- app/controllers/search.rb
+++ app/controllers/search.rb
@@ -1,7 +1,7 @@
 get '/search/anime/:query' do
   pass if params['query'] == "genres"
   endpoint = "/anime/search/#{encode_string(params['query'])}"
-  @search_results = get_response_body(settings.anilist_communicator.make_get_request(endpoint))
+  @search_results = get_response_body(settings.anilist_communicator.make_get_request(endpoint)).select {|entry| entry['genres'].none? {|genre| genre == "Hentai"} }
   @search_phrase = params['query']
   @header = erb :header
   @footer = erb :footer
