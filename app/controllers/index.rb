3867e0af5e83bb5700a67ae041b8be9d72234e0a
Anthony Cook <ddragonmaster49@yahoo.com>
2017-03-13 17:04:55 +0000

    Show top rated anime on index page

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,23 @@
 get '/' do
-  @site_search_partial = erb :anime_title_search_partial
+  pass if (params['page'] != nil && !(params['page'].codepoints.all? {|codepoint| ((48..57) === codepoint) }))
+  
+  url = "/browse/anime?sort=score&page=#{ params['page'] || 1 }&genres_exclude=Hentai"
+  res = settings.anilist_communicator.make_get_request(url)
+  pass if !res.kind_of? Net::HTTPSuccess
+  
+  @search_results = get_response_body(res)
+  
+  if !@search_results.empty?
+    pagination_test_url = "/browse/anime?sort=score&page=#{ (params['page']) ? params['page'].to_i + 1 : 2 }&genres_exclude=Hentai"
+    test_result = get_response_body(settings.anilist_communicator.make_get_request(pagination_test_url))
+    @is_previous_button_visible = (params['page'].nil? || params['page'].to_i == 1) ? false : true
+    @is_next_button_visible = (Array === test_result && !test_result.empty?) ? true : false
+  else
+    pass
+  end
+  
+  @pageNumber = (params['page']) ? params['page'].to_i : 1
+  @title_search_partial = erb :anime_title_search_partial
   @header = erb :header
   @footer = erb :footer
   
